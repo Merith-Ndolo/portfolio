f17710bc489faadeda374af337e52db062f330fb
Merith <merithndolo@gmail.com>
2022-10-27 01:02:40 +0200

    style: update

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -17,7 +17,7 @@ const Hero = (props) => (
         <SectionTitle main center>
           Hey salut,
         </SectionTitle>
-        <div style={{ fontSize: "24px", marginBottom: "5px" }}>
+        <div style={{ fontSize: "24px", marginBottom: "6px" }}>
           <TypingEffect
             staticText="Je suis"
             text={[
